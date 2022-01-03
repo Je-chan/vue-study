e7e68eb5aced0b4d7f672b09318aca86e3f0353a
박예찬 <jkyyc3@gmail.com>
2022-01-03 21:41:36 +0900

    Computed 학습

--- practice/src/App.vue
+++ practice/src/App.vue
@@ -2,18 +2,30 @@
   <div>
     <Lifecycle />
     <Syntax />
+    <Computed />
   </div>
 </template>
 
 <script>  
 import Lifecycle from './components/Lifecycle'
 import Syntax from './components/Syntax'
+import Computed from './components/Computed'
 
 export default { 
   
   components: {
     Lifecycle,
-    Syntax
+    Syntax,
+    Computed
   }
 }
 </script>
+
+<style>
+  div{
+    margin: 2rem 1rem;
+  }
+  h1 {
+    font-size: 2rem;
+  }
+</style>
---
+++ practice/src/components/Computed.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <section v-if='hasFruit'>
+      <h1>Fruits</h1>
+      <ul>
+        <li v-for='fruit in fruits' :key='fruit'>
+          {{fruit}}
+        </li>
+      </ul>
+    </section>
+    <section>
+      <h1>Reversed</h1>
+      <ul>
+        <li v-for='fruit in reverseFruits' :key='fruit'>
+          {{fruit}}
+        </li>
+      </ul>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Computed',
+  data() {
+    return {
+      fruits: [
+      'Apple', 'Banana', 'Cherry'
+      ]
+    }
+  },
+  computed: {
+    // computed 를 통해서 계산된 데이터를 저장. 
+    hasFruit() {
+      // this 는 default 다음에 나오는 객체 내부를 의미
+      return this.fruits.length > 0
+    },
+    reverseFruits() {
+      return this.fruits.map(fruit => {
+        return fruit.split('').reverse().join('')
+      })
+    }
+  }
+}
+</script>
