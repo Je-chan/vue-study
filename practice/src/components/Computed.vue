36f836149b2d9cd47744e615bc5e145bc2f572d8
박예찬 <jkyyc3@gmail.com>
2022-01-03 21:54:53 +0900

    Computed 를 통한 캐싱 학습

--- practice/src/components/Computed.vue
+++ practice/src/components/Computed.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <section v-if='hasFruit'>
-      <h1>Fruits</h1>
+      <!--이렇게 하면 아쉬운 점은 같은 로직인데 세 번 로직이 실행된다는 점 -->
+      <h1>{{ reverseMessage() }}</h1>
+      <h1>{{ reverseMessage() }}</h1>
+      <h1>{{ reverseMessage() }}</h1>
       <ul>
         <li v-for='fruit in fruits' :key='fruit'>
           {{fruit}}
@@ -9,7 +12,10 @@
       </ul>
     </section>
     <section>
-      <h1>Reversed</h1>
+      <!-- computed 를 사용해 값을 캐싱해서 사용할 수 있음. computed 에 저장될 때 로직이 딱 한 번만 실행된다. -->
+      <h1>{{reverseTitleMsg}}</h1>
+      <h1>{{reverseTitleMsg}}</h1>
+      <h1>{{reverseTitleMsg}}</h1>
       <ul>
         <li v-for='fruit in reverseFruits' :key='fruit'>
           {{fruit}}
@@ -24,9 +30,16 @@ export default {
   name: 'Computed',
   data() {
     return {
+      title: 'stiurF',
+      reversedTitle: 'Fruits',
       fruits: [
       'Apple', 'Banana', 'Cherry'
       ]
+    }
+  },
+  methods: {
+    reverseMessage () {
+      return this.title.split('').reverse().join('')
     }
   },
   computed: {
@@ -39,7 +52,16 @@ export default {
       return this.fruits.map(fruit => {
         return fruit.split('').reverse().join('')
       })
+    },
+    reverseTitleMsg () {
+      return this.reversedTitle.split('').reverse().join('')
     }
   }
 }
 </script>
+
+<style scoped>
+  section {
+    margin: 1rem 0;
+  }
+</style>
