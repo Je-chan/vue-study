8dd20cf96528cefc5a9c518f80fbd224921a4e23
박예찬 <jkyyc3@gmail.com>
2022-01-03 22:12:37 +0900

    Computed 를 활용한 getter, setter 사용 학습

--- practice/src/components/Computed.vue
+++ practice/src/components/Computed.vue
@@ -22,6 +22,12 @@
         </li>
       </ul>
     </section>
+    <section>
+      <button @click='add'>add '?!' </button>
+      <h1>Getter : {{reverseTitleMsg}}</h1>
+      <h1>{{setterTest}}</h1>
+      <h1>{{computedSetter}}</h1>
+    </section>
   </div>
 </template>
 
@@ -32,6 +38,8 @@ export default {
     return {
       title: 'stiurF',
       reversedTitle: 'Fruits',
+      setterTest: 'Getter and Setter',
+      computedSetterTest: 'detupmoc morf retteG',
       fruits: [
       'Apple', 'Banana', 'Cherry'
       ]
@@ -40,6 +48,20 @@ export default {
   methods: {
     reverseMessage () {
       return this.title.split('').reverse().join('')
+    },
+    add() {
+      this.reverseTitleMsg += '?!'
+      // 이건 불가능하다. computed 로 저장한 것은 읽기 전용이기 때문
+      // Getter : 값을 가져오는 방식으로만 사용하는 것.
+      this.setterTest += '?!'
+      // 반면에 data 로 저장된 것은 setter 까지 포함이 된다. 
+      this.computedSetter += '?!'
+      // computed 도 getter 와 setter 둘 다 사용할 수 있다.
+      // get() 을 통해서 값을 가져오는 거고
+      // set() 을 통해서 값을 변형시킬 때 실행될 함수를 만드는 것
+      // 여기에서 ?! 를 추가하고 그게 set(newValue) 의 newValue 값으로 들어가게 된디.
+      // 그렇게 set 함수가 실행된 다음에 화면에 렌더링할 때는 get() 의 내용에 따라 실행
+      // 많이 쓰이지는 않지만 Vuex 라는 중앙 집중식 저장소를 사용할 때 유용하게 사용할 수 있다.
     }
   },
   computed: {
@@ -55,6 +77,15 @@ export default {
     },
     reverseTitleMsg () {
       return this.reversedTitle.split('').reverse().join('')
+    },
+    computedSetter: {
+      get() {
+        return this.computedSetterTest.split('').reverse().join('')
+      }, 
+      set(newValue) {
+        console.log('newValue', newValue)
+        this.computedSetterTest = newValue
+      }
     }
   }
 }
